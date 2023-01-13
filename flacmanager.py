0d7eb60b9b16d991952f5ab162f4ea88ce2c8347
Rodrigue Gaspard <rodriguegaspard@gmail.com>
2023-01-13 19:29:54 +0100

    Modified printMetaData to print empty tags

--- flacmanager.py
+++ flacmanager.py
@@ -2,10 +2,16 @@ import os
 import mutagen
 import argparse
 
-def printMetaData(audio_files):
+def printMetadata(audio_files):
     print ("{:<40} {:<20} {:<30} {:<10} {:<30}".format('Album', 'Genre', 'Artist', '#', 'Title'))
     for file in audio_files:
-        print("{:<40} {:<20} {:<30} {:<10} {:<30}".format(file.tags["album"][0], file.tags["genre"][0], file.tags["artist"][0], file.tags["tracknumber"][0], file.tags["title"][0]))
+        album = file.tags["album"][0] if "album" in file.tags else "N/A"
+        genre = file.tags["genre"][0] if "genre" in file.tags else "N/A"
+        artist = file.tags["artist"][0] if "artist" in file.tags else "N/A"
+        tracknumber = file.tags["tracknumber"][0] if "tracknumber" in file.tags else "N/A"
+        title = file.tags["title"][0] if "title" in file.tags else "N/A"
+        print("{:<40} {:<20} {:<30} {:<10} {:<30}".format(album, genre, artist, tracknumber, title))
+
 
 # Creating the parser
 parser = argparse.ArgumentParser(description='Manages metadata for multiple audio formats.')
@@ -13,10 +19,7 @@ parser.add_argument("input", metavar="files", nargs="+", help='audio file(s)')
 parser.add_argument("-l", "--list", action="store_true", default=False, help='Prints the metadata of the audio files.')
 args = parser.parse_args()
 
-# Access the input arguments
-audio_files = map(lambda file: mutagen.File(file), args.input)
 
 if args.list:
-        printMetaData(audio_files)
-else:
-    print("Nothing to do.")
+    printMetadata(audio_files)
+
