2a1d4b003c36259edebb2a3c4aa4072ddb3b1619
Rodrigue Gaspard <rodriguegaspard@gmail.com>
2023-01-16 21:51:14 +0100

    Added -c/--check flag to print metadata issues

--- flacmanager.py
+++ flacmanager.py
@@ -2,6 +2,24 @@ import os
 import mutagen
 import argparse
 
+def printMetadataIssues(audio_files):
+    tags = ["album" , "genre", "artist", "tracknumber", "title"]
+    badFiles = 0
+    for file in audio_files:
+        issues = 0
+        issuesList = ""
+        for tag in tags:
+            if tag not in file[0].tags:
+                issues+=1
+                issuesList+=tag
+        if len(file[0].pictures) < 1:
+            issues+=1
+            issuesList+="album_cover"
+        if issues>0:
+            badFiles+=1
+            print("{} issue(s) found for \'{}\' ({}).".format(issues, file[1], issuesList))
+    print("{} file(s) with metadata issues found.".format(badFiles))
+
 def printMetadata(audio_files):
     print ("{:<40} {:<20} {:<30} {:<10} {:<30}".format('Album', 'Genre', 'Artist', '#', 'Title'))
     for file in audio_files:
@@ -16,6 +34,7 @@ def printMetadata(audio_files):
 parser = argparse.ArgumentParser(description='Manages metadata for multiple audio formats.')
 parser.add_argument("input", metavar="files", nargs="+", help='audio file(s)')
 parser.add_argument("-l", "--list", action="store_true", default=False, help='Prints the metadata of the audio files.')
+parser.add_argument("-c", "--check", action="store_true", default=False, help='Prints metadata issues (missing tags or album covers).')
 args = parser.parse_args()
 
 # Access the input arguments, and removes any unwanted files
@@ -25,3 +44,4 @@ if args.list:
     printMetadata(audio_files)
 
 if args.check:
+    printMetadataIssues(audio_files)
